96a68985a09188f3744af4cc1d4faaea74048d5f
palleakshaya <akshaya.palle@proclink.com>
2024-08-24 18:15:22 +0530

    login, logout working only if refresh the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,34 @@ import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './auth.service';
 // import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatDividerModule, MatButtonModule],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular_test';
+  isLoggedIn: boolean = false;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   // private roleId = 1;
-  constructor() {}
+  constructor(private router: Router) {}
   getToken(): string | null {
     return localStorage.getItem('token');
   }
@@ -19,9 +20,19 @@ export class AuthService {
     // return !!token;
     return !!localStorage.getItem('token');
   }
+
   isAdmin(): boolean {
     const roleIdStr = localStorage.getItem('roleId');
     const roleId = roleIdStr ? Number(roleIdStr) : 1;
     return roleId === 0; // Return true if the role is 0 (admin)
+  }
+  logout() {
+    // Remove token and other user data from storage
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    localStorage.removeItem('roleId');
+
+    // Redirect user to login page or home page
+    this.router.navigate(['/login']);
   }
 }
